2df7a020cbb28f55d492d8ee7744b9e393ee7de9
soyoongdev <huuhau.hh47@gmail.com>
2023-09-12 20:46:25 +0700

    install and config: Prettier and Eslint

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import helmet from 'helmet'
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
+
+app.use(helmet())
+
 const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
